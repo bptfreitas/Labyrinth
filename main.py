6c839f8420892dd6a60e4bb6bbf74f79c9eefa0f
Bruno <bruno.freitas@cefet-rj.br>
2024-10-03 16:16:47 -0300

    Started the Webots driver

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import sys
+
+sys.path.append('.')
+
+import class_map_creator as mc
+import drivers.class_base_map_driver as bm
+import drivers.webots.class_webots_driver as webots
+
+import class_map_creator as mc
+
+dir( bm )
+dir( mc )
+
+mapDriver = webots.WebotsDriver("teste.wbt")
+
+map = mc.Map( mapDriver  )
