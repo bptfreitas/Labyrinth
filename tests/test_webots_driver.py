004773c1e321c69809e66c6634057a1bfecc32cf
Bruno <brunopolicarpo@hotmail.com>
2025-09-22 17:31:12 -0300

    First map generation test OK

--- tests/test_webots_driver.py
+++ tests/test_webots_driver.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 import sys
+import shutil
+import os
+import subprocess as sp
 
 sys.path.append('../')
 
@@ -17,6 +20,17 @@ class TestMap(unittest.TestCase):
     def test_start(self):
 
         unittest.SkipTest()
+
+    def test_map_generation(self):
+
+        for map in sorted( os.listdir("./maps") ):
+            print(map)
+
+            proc= sp.run( [ './main.py', '-i',  './maps/' + map ] ,\
+                stdout = sp.DEVNULL ,\
+                stderr = sp.STDOUT )
+
+            self.assertEqual( proc.returncode , 0 )
 
 
 if __name__ == '__main__' :
